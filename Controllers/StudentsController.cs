1916feb7b487e5059de099cbff0c9c2ed18c15f8
mannan49 <mannannasir49@gmail.com>
2024-09-04 09:05:38 +0500

    Initial commit

---
+++ Controllers/StudentsController.cs
@@ -0,0 +1,15 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class StudentsController:ControllerBase
+    {
+        [HttpGet]
+        public string SayHi()
+        {
+            return "Hi";
+        }
+    }
+}
